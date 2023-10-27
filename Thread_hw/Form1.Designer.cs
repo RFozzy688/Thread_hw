f71920e027049e1b1543963ee1c0d42c84ae3f24
RFozzy688 <pokerfozzy@gmail.com>
2023-10-27 03:22:11 +0300

    start/abort thread PrimeNumber

--- Thread_hw/Form1.Designer.cs
+++ Thread_hw/Form1.Designer.cs
@@ -28,13 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.StartPrime = new System.Windows.Forms.Button();
+            this.StopPrime = new System.Windows.Forms.Button();
+            this.PrimeNumberTextBox = new System.Windows.Forms.TextBox();
+            this.numericPrimeMin = new System.Windows.Forms.NumericUpDown();
+            this.numericPrimeMax = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericPrimeMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericPrimeMax)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericPrimeMax);
+            this.groupBox1.Controls.Add(this.numericPrimeMin);
+            this.groupBox1.Controls.Add(this.PrimeNumberTextBox);
+            this.groupBox1.Controls.Add(this.StopPrime);
+            this.groupBox1.Controls.Add(this.StartPrime);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(214, 388);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Простые числа";
+            // 
+            // StartPrime
+            // 
+            this.StartPrime.Location = new System.Drawing.Point(18, 263);
+            this.StartPrime.Name = "StartPrime";
+            this.StartPrime.Size = new System.Drawing.Size(75, 23);
+            this.StartPrime.TabIndex = 1;
+            this.StartPrime.Text = "Start";
+            this.StartPrime.UseVisualStyleBackColor = true;
+            this.StartPrime.Click += new System.EventHandler(this.StartPrime_Click);
+            // 
+            // StopPrime
+            // 
+            this.StopPrime.Location = new System.Drawing.Point(119, 263);
+            this.StopPrime.Name = "StopPrime";
+            this.StopPrime.Size = new System.Drawing.Size(75, 23);
+            this.StopPrime.TabIndex = 2;
+            this.StopPrime.Text = "Stop";
+            this.StopPrime.UseVisualStyleBackColor = true;
+            this.StopPrime.Click += new System.EventHandler(this.StopPrime_Click);
+            // 
+            // PrimeNumberTextBox
+            // 
+            this.PrimeNumberTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.PrimeNumberTextBox.Location = new System.Drawing.Point(18, 19);
+            this.PrimeNumberTextBox.Multiline = true;
+            this.PrimeNumberTextBox.Name = "PrimeNumberTextBox";
+            this.PrimeNumberTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.PrimeNumberTextBox.Size = new System.Drawing.Size(176, 81);
+            this.PrimeNumberTextBox.TabIndex = 1;
+            // 
+            // numericPrimeMin
+            // 
+            this.numericPrimeMin.Location = new System.Drawing.Point(18, 224);
+            this.numericPrimeMin.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericPrimeMin.Name = "numericPrimeMin";
+            this.numericPrimeMin.Size = new System.Drawing.Size(75, 20);
+            this.numericPrimeMin.TabIndex = 1;
+            this.numericPrimeMin.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // numericPrimeMax
+            // 
+            this.numericPrimeMax.Location = new System.Drawing.Point(119, 224);
+            this.numericPrimeMax.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericPrimeMax.Name = "numericPrimeMax";
+            this.numericPrimeMax.Size = new System.Drawing.Size(75, 20);
+            this.numericPrimeMax.TabIndex = 3;
+            this.numericPrimeMax.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(484, 412);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericPrimeMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericPrimeMax)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button StopPrime;
+        private System.Windows.Forms.Button StartPrime;
+        private System.Windows.Forms.NumericUpDown numericPrimeMin;
+        private System.Windows.Forms.TextBox PrimeNumberTextBox;
+        private System.Windows.Forms.NumericUpDown numericPrimeMax;
     }
 }
 
--- Thread_hw/Form1.cs
+++ Thread_hw/Form1.cs
@@ -5,16 +5,58 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
 
 namespace Thread_hw
 {
     public partial class Form1 : Form
     {
+        Thread thread;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void StartPrime_Click(object sender, EventArgs e)
+        {
+            ThreadStart threadStart = new ThreadStart(GeneratePrimeNumbers);
+            thread = new Thread(threadStart);
+            thread.Start();
+        }
+        void GeneratePrimeNumbers()
+        {
+            string str = "";
+            int pos = 0;
+
+            for (int x = (int)numericPrimeMin.Value; x < (int)numericPrimeMax.Value; x++)
+            {
+                if (PrimeNumber(x))
+                {
+                    //str += x.ToString() + "\r\n";
+                    PrimeNumberTextBox.AppendText(x.ToString() + "\r\n");
+                }
+
+                Thread.Sleep(500);
+            }
+        }
+        static bool PrimeNumber(int x)
+        {
+            for (int d = 2; d <= Math.Sqrt(x); d++)
+            {
+                if (x % d == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void StopPrime_Click(object sender, EventArgs e)
+        {
+            thread.Abort();
         }
     }
 }
